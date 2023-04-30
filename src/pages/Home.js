ee67b17be0aa77a39c7264f605b0ab45b71a6e77
Sena Lim <tpsk8322@naver.com>
2023-05-01 03:29:21 +0900

    Link 컴포넌트를 사용하여 중첩된 라우트 설정

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,6 +10,7 @@ const Home = () => {
                 <li><Link to="/profiles/velopert">velopert의 프로필</Link></li>
                 <li><Link to="/profiles/sena">sena의 프로필</Link></li>
                 <li><Link to="/profiles/void">존재하지 않는 프로필</Link></li>
+                <li><Link to="/articles">게시글 목록</Link></li>
             </ul>
         </div>
     );
