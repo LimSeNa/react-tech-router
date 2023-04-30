e569ca7a85f50264faacc2f64529befa44facd49
Sena Lim <tpsk8322@naver.com>
2023-05-01 03:57:23 +0900

    중첩된 라우트와 Outlet 컴포넌트를 사용하여 공통 레이아웃 컴포넌트 렌더링

--- src/App.js
+++ src/App.js
@@ -4,15 +4,18 @@ import About from "./pages/About";
 import Profile from "./pages/Profile";
 import Articles from "./pages/Articles";
 import Article from "./pages/Article";
+import Layout from "./Layout";
 
 const App = () => {
     return (
         <Routes>
-            <Route path="/" element={<Home/>}/>
-            <Route path="/about" element={<About/>}/>
-            <Route path="/profiles/:username" element={<Profile/>}/>
-            <Route path="/articles" element={<Articles />}>
-                <Route path=":id" element={<Article />}/>
+            <Route element={<Layout/>}>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/about" element={<About/>}/>
+                <Route path="/profiles/:username" element={<Profile/>}/>
+            </Route>
+            <Route path="articles" element={<Articles/>}>
+                <Route path=":id" element={<Article/>}/>
             </Route>
         </Routes>
     );
