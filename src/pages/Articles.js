a1bcc7963c00df8d381e28160aa8d66d433733d4
Sena Lim <tpsk8322@naver.com>
2023-05-01 04:41:24 +0900

    리액트 라우터 부가 기능 - NavLink 컴포넌트

--- src/pages/Articles.js
+++ src/pages/Articles.js
@@ -1,13 +1,41 @@
-import {Link, Outlet} from "react-router-dom";
+import {NavLink, Outlet} from "react-router-dom";
 
 const Articles = () => {
+    const activeStyle = {
+        color: 'pink',
+        fontSize: 21
+    };
+
     return (
-        <ul>
+        <div>
             <Outlet/>
-            <li><Link to="/articles/1">게시글 1</Link></li>
-            <li><Link to="/articles/2">게시글 2</Link></li>
-            <li><Link to="/articles/3">게시글 3</Link></li>
-        </ul>
+            <ul>
+                <li>
+                    <NavLink
+                        to="/articles/1"
+                        style={({isActive}) => (isActive ? activeStyle : undefined)}
+                    >
+                        게시글 1
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        to="/articles/2"
+                        style={({isActive}) => (isActive ? activeStyle : undefined)}
+                    >
+                        게시글 2
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        to="/articles/3"
+                        style={({isActive}) => (isActive ? activeStyle : undefined)}
+                    >
+                        게시글 3
+                    </NavLink>
+                </li>
+            </ul>
+        </div>
     );
 };
 
